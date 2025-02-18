332f4a03acdb0189863e1d1483d9148dcdb43bfa
Vihaan Chhabria <72771042+VihaanChhabria@users.noreply.github.com>
2025-02-18 16:16:33 -0500

    Update RobotContainer.java

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,15 +4,18 @@
 
 package frc.robot;
 
-
 import static frc.robot.subsystems.Vision.VisionConstants.*;
 
 import org.littletonrobotics.junction.networktables.LoggedDashboardChooser;
 import com.pathplanner.lib.auto.AutoBuilder;
+import com.pathplanner.lib.path.PathConstraints;
+
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
+import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.commands.DriveCommands;
@@ -34,9 +37,12 @@ import frc.robot.subsystems.Vision.VisionIOLimelight;
 import frc.robot.subsystems.Vision.VisionIOPhotonVisionSim;
 
 /**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * This class is where the bulk of the robot should be declared. Since
+ * Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in
+ * the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of
+ * the robot (including
  * subsystems, commands, and trigger mappings) should be declared here.
  */
 public class RobotContainer {
@@ -46,56 +52,63 @@ public class RobotContainer {
   private final Elevator elevator;
 
   private final CommandXboxController driverController = new CommandXboxController(0);
-  //private final CommandXboxController operatorController = new CommandXboxController(1);
+  
+  private final Trigger joystickMoveTrigger = new Trigger(() -> isJoystickMoved());
+  // private final CommandXboxController operatorController = new
+  // CommandXboxController(1);
 
   private final LoggedDashboardChooser<Command> autoChooser;
 
-  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  /**
+   * The container for the robot. Contains subsystems, OI devices, and commands.
+   */
   public RobotContainer() {
     switch (Constants.currentMode) {
       case REAL:
-        drive = 
-          new Drive(
+        drive = new Drive(
             new GyroIONavx(),
             new ModuleIOSparkMax(0),
             new ModuleIOSparkMax(1),
             new ModuleIOSparkMax(2),
             new ModuleIOSparkMax(3));
-        vision = 
-          new Vision(
+        vision = new Vision(
             drive::addVisionMeasurement,
             new VisionIOLimelight("limelight", drive::getRotation));
-        elevator = 
-          new Elevator(new ElevatorIOSim());
-        
+        elevator = new Elevator(new ElevatorIOSim());
+
         break;
 
-      case SIM: 
-        drive = 
-          new Drive(
-            new GyroIO() {},
+      case SIM:
+        drive = new Drive(
+            new GyroIO() {
+            },
             new ModuleIOSim(),
             new ModuleIOSim(),
             new ModuleIOSim(),
             new ModuleIOSim());
         vision = new Vision(
-          drive::addVisionMeasurement,
-          new VisionIOPhotonVisionSim("limelight", robotToCamera, drive::getPose));
-        elevator = 
-          new Elevator(
+            drive::addVisionMeasurement,
+            new VisionIOPhotonVisionSim("limelight", robotToCamera, drive::getPose));
+        elevator = new Elevator(
             new ElevatorIOSim());
         break;
-    
+
       default:
-        drive = 
-          new Drive(
-            new GyroIO() {},
-            new ModuleIO() {},
-            new ModuleIO() {},
-            new ModuleIO() {},
-            new ModuleIO() {});
-        vision = new Vision(drive::addVisionMeasurement, new VisionIO() {});
-        elevator = new Elevator(new ElevatorIO() {});
+        drive = new Drive(
+            new GyroIO() {
+            },
+            new ModuleIO() {
+            },
+            new ModuleIO() {
+            },
+            new ModuleIO() {
+            },
+            new ModuleIO() {
+            });
+        vision = new Vision(drive::addVisionMeasurement, new VisionIO() {
+        });
+        elevator = new Elevator(new ElevatorIO() {
+        });
         break;
     }
 
@@ -104,38 +117,52 @@ public class RobotContainer {
   }
 
   /**
-   * Use this method to define your trigger->command mappings. Triggers can be created via the
-   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * Use this method to define your trigger->command mappings. Triggers can be
+   * created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with
+   * an arbitrary
    * predicate, or via the named factories in {@link
-   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
-   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
-   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for
+   * {@link
+   * CommandXboxController
+   * Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or
+   * {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
    * joysticks}.
    */
   private void configureBindings() {
-    drive.setDefaultCommand(
-      DriveCommands.FPSDrive(
-        drive,
-        () -> -driverController.getLeftY(),
-        () -> -driverController.getLeftX(),
-        () -> -driverController.getRightX()));
-    //m_driverController.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
+      drive.setDefaultCommand(
+          DriveCommands.FPSDrive(
+              drive,
+              () -> -driverController.getLeftY(),
+              () -> -driverController.getLeftX(),
+              () -> -driverController.getRightX()));
+    
+    Command pathfindPath = AutoBuilder.pathfindToPose(
+                new Pose2d(10, 5, Rotation2d.fromDegrees(180)),
+                new PathConstraints(
+                    3.0, 4.0,
+                    Units.degreesToRadians(540), Units.degreesToRadians(720)));
+
+    driverController.rightStick().onTrue(pathfindPath);
+    joystickMoveTrigger.whileTrue(new InstantCommand(()-> pathfindPath.cancel()));
+    // m_driverController.x().onTrue(Commands.runOnce(drive::stopWithX, drive));
     driverController
-      .b()
+        .b()
         .onTrue(
-          Commands.runOnce(
-            () -> 
-              drive.setPose(
-                new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
-            drive)
-          .ignoringDisable(true));
-    /* 
-          elevator.setDefaultCommand(
-      new ManualElevatorCommand(
-        elevator,
-        () -> operatorController.getLeftY()));
-    */
-    //operatorController.axisGreaterThan(0, 0).onTrue(ElevatorCommands.testElevator(elevator));
+            Commands.runOnce(
+                () -> drive.setPose(
+                    new Pose2d(drive.getPose().getTranslation(), new Rotation2d())),
+                drive)
+                .ignoringDisable(true));
+    /*
+     * elevator.setDefaultCommand(
+     * new ManualElevatorCommand(
+     * elevator,
+     * () -> operatorController.getLeftY()));
+     */
+    // operatorController.axisGreaterThan(0,
+    // 0).onTrue(ElevatorCommands.testElevator(elevator));
     // driverController.a().whileTrue(DriveCommands.feedforwardCharacterization(drive));
     // m_driverController.a().whileTrue(drive.sysIdDynamic(SysIdRoutine.Direction.kForward));
     // m_driverController.x().whileTrue(drive.sysIdDynamic(SysIdRoutine.Direction.kReverse));
@@ -151,5 +178,13 @@ public class RobotContainer {
   public Command getAutonomousCommand() {
     // An example command will be run in autonomous
     return autoChooser.get();
+  }
+
+  private boolean isJoystickMoved() {
+    // Check if there's significant joystick movement
+    return Math.abs(driverController.getLeftY()) > 0.1 ||
+        Math.abs(driverController.getLeftX()) > 0.1 ||
+        Math.abs(driverController.getRightX()) > 0.1 ||
+        Math.abs(driverController.getRightY()) > 0.1;
   }
 }

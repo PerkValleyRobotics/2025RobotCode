35a8a09e1142e72d67ba3a685dee22c918dbe960
Vihaan Chhabria <72771042+VihaanChhabria@users.noreply.github.com>
2025-03-14 08:31:26 -0400

    added io stuff for sensor

--- src/main/java/frc/robot/subsystems/CoralSensor/CoralSensorIOReal.java
+++ src/main/java/frc/robot/subsystems/CoralSensor/CoralSensorIOReal.java
@@ -1,25 +1,20 @@
-// // Copyright (c) FIRST and other WPILib contributors.
-// // Open Source Software; you can modify and/or share it under the terms of
-// // the WPILib BSD license file in the root directory of this project.
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
 
-// package frc.robot.subsystems.CoralSensor;
+package frc.robot.subsystems.CoralSensor;
 
 
-// import com.revrobotics.Rev2mDistanceSensor;
-// import com.revrobotics.Rev2mDistanceSensor.Port;
-// import com.revrobotics.Rev2mDistanceSensor.RangeProfile;
+/** Add your docs here. */
+public class CoralSensorIOReal implements CoralSensorIO {
+    private final DigitalInput sensor;
+    private final int SENSOR_PORT = 0;
 
-// /** Add your docs here. */
-// public class CoralSensorIOReal implements CoralSensorIO {
-//     private final Rev2mDistanceSensor sensor;
+    public CoralSensorIOReal() {
+        sensor = new DigitalInput(SENSOR_PORT);
+    }
 
-//     public CoralSensorIOReal() {
-//         sensor = new Rev2mDistanceSensor(Port.kMXP);
-//         sensor.setAutomaticMode(true);
-//         sensor.setRangeProfile(RangeProfile.kHighAccuracy);
-//     }
-
-//     public void updateInputs(CoralIOInputs inputs) {
-//         inputs.distance = sensor.getRange();
-//     }
-// }
+    public void updateInputs(CoralSensorIOInputs inputs) {
+        inputs.state = sensor.get();
+    }
+}

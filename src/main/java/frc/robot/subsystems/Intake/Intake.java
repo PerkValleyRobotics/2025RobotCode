517e33ca1fc3a9ff0f1fffb75e1b344a2b24828d
Retrorobotics <perkiomenvalleyrobotics@gmail.com>
2025-03-13 21:03:43 -0400

    merge 2

--- src/main/java/frc/robot/subsystems/Intake/Intake.java
+++ src/main/java/frc/robot/subsystems/Intake/Intake.java
@@ -2,37 +2,37 @@ package frc.robot.subsystems.Intake;
 
 import static frc.robot.subsystems.Intake.IntakeConstants.*;
 
-// import org.littletonrobotics.junction.AutoLogOutput;
-// import org.littletonrobotics.junction.Logger;
+import org.littletonrobotics.junction.AutoLogOutput;
+import org.littletonrobotics.junction.Logger;
 
-// import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class Intake extends SubsystemBase {
     private IntakeIO io;
     private final IntakeIOInputsAutoLogged inputs = new IntakeIOInputsAutoLogged();
 
-// private double setpoint;
+private double setpoint;
 
     public Intake(IntakeIO io) {
         this.io = io;
     }
 
-// @Override
-// public void periodic() {
-// io.updateInputs(inputs);
-// Logger.processInputs("EndEffector", inputs);
-// }
+@Override
+public void periodic() {
+io.updateInputs(inputs);
+Logger.processInputs("EndEffector", inputs);
+}
 
-// public void goBack() {
-// setpoint = CLIMB_SETPOINT;
-// }
+public void goBack() {
+setpoint = CLIMB_SETPOINT;
+}
 
-// public void goHome() {
-// setpoint = HOME_SETPOINT;
-// }
+public void goHome() {
+setpoint = HOME_SETPOINT;
+}
 
-// @AutoLogOutput(key = "Insteak/Setpoint")
-// public double getCurrentSetpoint() {
-// return setpoint;
-// }
-// }
+@AutoLogOutput(key = "Insteak/Setpoint")
+public double getCurrentSetpoint() {
+return setpoint;
+}
+}

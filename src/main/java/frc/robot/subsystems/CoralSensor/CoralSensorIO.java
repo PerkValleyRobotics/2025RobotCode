345102ca608088ae6975bec1a23f63fc46e2ad39
Vihaan Chhabria <72771042+VihaanChhabria@users.noreply.github.com>
2025-03-18 13:24:25 -0400

    Update CoralSensorIO.java to need a stable duration

--- src/main/java/frc/robot/subsystems/CoralSensor/CoralSensorIO.java
+++ src/main/java/frc/robot/subsystems/CoralSensor/CoralSensorIO.java
@@ -1,17 +1,33 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot.subsystems.CoralSensor;
 
-import org.littletonrobotics.junction.AutoLog;
+import edu.wpi.first.wpilibj.DigitalInput;
+import edu.wpi.first.wpilibj.Timer;
 
-public interface CoralSensorIO {
-    @AutoLog
-    public static class CoralSensorIOInputs {
-        public boolean state = false;
+public class CoralSensorIOReal implements CoralSensorIO {
+    private final DigitalInput sensor;
+    private final int SENSOR_PORT = 1;
+    private boolean lastValidState;
+    private double lastStateChangeTime;
+    private static final double STABLE_DURATION = 0.15; // 100ms debounce time
+
+    public CoralSensorIOReal() {
+        sensor = new DigitalInput(SENSOR_PORT);
+        lastValidState = sensor.get();
+        lastStateChangeTime = Timer.getFPGATimestamp();
     }
 
-    public default void updateInputs(CoralSensorIOInputs inputs) {
+    public void updateInputs(CoralSensorIOInputs inputs) {
+        boolean currentState = sensor.get();
+        double currentTime = Timer.getFPGATimestamp();
+
+        if (currentState != lastValidState) {
+            lastStateChangeTime = currentTime; // Reset timer when state changes
+        }
+
+        if ((currentTime - lastStateChangeTime) >= STABLE_DURATION) {
+            lastValidState = currentState; // Accept the new state after it remains stable
+        }
+
+        inputs.state = lastValidState;
     }
 }
